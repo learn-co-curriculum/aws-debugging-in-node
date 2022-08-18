b56b2061d041d465634fac9ec18f2d3179732c7e
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-08-18 10:29:36 -0700

    initialize files

---
+++ index.js
@@ -0,0 +1,9 @@
+let count = 1;
+while (count < 5) {
+  if (count === 1) {
+    console.log(`The loop has executed ${count} time.`);
+  } else {
+    console.log(`The loop has executed ${count} times.`);
+  }
+  count += 1;
+}
